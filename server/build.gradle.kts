a75ddbb205f09a23b7c81f0aaf83f948b341946c
Yasashnyy Maxim <max.yas@list.ru>
2023-11-10 18:39:21 +0300

    feat(server): Add serialization

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
