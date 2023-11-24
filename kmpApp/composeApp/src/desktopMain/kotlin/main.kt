8a8909c85b5d80f167f7277f235eb8d332b0c3aa
Yasashnyy Maxim <max.yas@list.ru>
2023-11-24 18:58:17 +0300

    feat (kmpApp): Add material 3

--- kmpApp/composeApp/src/desktopMain/kotlin/main.kt
+++ kmpApp/composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,23 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.example.compose.AppTheme
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        AppTheme {
+            Surface (
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ){
+                App()
+            }
+        }
     }
 }
 
